22c85558a96db23fcbbf394cc142b3381558d5c4
Jakub Gabryel <j.gabryel@DIGITAL.local>
2018-01-04 15:01:00 +0100

    Refactorization and language selector added

--- data.py
+++ data.py
@@ -12,6 +12,28 @@ def getmenutext(text, lang):
     new_text = cursor.fetchone()
     return new_text[0]
 
+def getstatsdesc(lang):
+    '''This function will return Array with all stats names in requested language'''
+    cursor = DBASE.cursor()
+    cursor.execute('''SELECT name FROM menus
+                    WHERE field_id IN ('ws','bs','s','t','ag','int','wp','fel') 
+                    AND lang=? ORDER BY position ASC;''', (lang,))
+    rows = cursor.fetchall()
+    result = []
+    for row in rows:
+        result.append(row[0])
+    return result
+
+def getlanguages():
+    '''This function will return all languages supported by the application'''
+    cursor = DBASE.cursor()
+    cursor.execute('''SELECT lang from languages''')
+    rows = cursor.fetchall()
+    result = []
+    for row in rows:
+        result.append(row[0])
+    return result
+
 def closedb():
     '''This functon closes db on app exit'''
     DBASE.close()
